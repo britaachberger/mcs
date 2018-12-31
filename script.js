964cf9f825cdad1473496008f8401c8d032e6324
Brita <brita.achberger@gmail.com>
2018-12-31 15:11:48 +0100

    task-9

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
-var X = parseInt(prompt("Asign Value to X", 0));
-console.log(X);
+const form = document.getElementById("form");
+var subscriptionPara = document.getElementById("thxSubscription");
+subscriptionPara.style.display="none"
 
-var Y = parseInt(prompt("Asign Value to Y", 0));
-console.log(Y);
+const JoinNewsletter = document.getElementById("btnJoinNewsletter");
+var valueJoinNewsletter = JoinNewsletter.value;
+console.log(valueJoinNewsletter);
 
 
-var SUM = (X + Y);
-console.log(SUM);
 
-alert(SUM)
+function checkEvent(event){
+  event.preventDefault();
+  console.log("Нажали");
+
+	var Input1 = document.getElementById("input1");
+	var valueInput1 = Input1.value;
+	console.log(valueInput1);
+
+
+	var Input2 = document.getElementById("input2");
+	var valueInput2 = Input2.value;
+	console.log(valueInput2);
+
+	  if(valueInput1, valueInput2 === ''){
+		console.log('No Login!');
+		if(valueInput1 === ''){
+			Input1.style.border = "2px solid red";
+			}
+		if(valueInput2 === ''){
+			Input2.style.border = "2px solid red";
+			}	
+		} else {
+		form.style.display = 'none';
+		
+		subscriptionPara.style.display="block";
+		JoinNewsletter.style.backgroundColor = "";
+	};
+}
+
+function backgroundRed(){JoinNewsletter.style.backgroundColor = "red";}
+function backgroundGreen(){JoinNewsletter.style.backgroundColor = "#A2CA29";}
+
+  JoinNewsletter.addEventListener('click', checkEvent);
+  JoinNewsletter.addEventListener('mousedown',backgroundRed);
+  JoinNewsletter.addEventListener('mouseup',backgroundGreen);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
